89f54b6a3edb3c87609bc7b1f55b1a1c84700b38
Vince Nibler <vince@lotuslabs.net>
2009-01-20 01:59:47 -0700

    to make specs pass add method parameter to the request

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -7,26 +7,26 @@ describe "/home/rps_calc" do
   end
 
   it "should convert to RPS by default" do
-    request("/home/rps_calc", :params => {:requests_per => 8_640_000}).should contain('reqs/day is 100 reqs/sec')
+    request("/home/rps_calc", :method => "POST", :params => {:requests_per => 8_640_000}).should contain('reqs/day is 100 reqs/sec')
   end
 
   it "should handle numbers with commas in them" do
-    request("/home/rps_calc", :params => {:requests_per => '8,640,000'}).should contain('100')
+    request("/home/rps_calc", :method => "POST", :params => {:requests_per => '8,640,000'}).should contain('100')
   end
 
   describe "converting into RPS" do
     it "should return 100 RPS for a input of 8.64 million" do
-      request("/home/rps_calc", :params => {:requests_per => 8_640_000}).should contain('100')
+      request("/home/rps_calc", :method => "POST", :params => {:requests_per => 8_640_000}).should contain('100')
     end
 
     it "should show an error message if requests_per is < 86,400" do
-      request("/home/rps_calc", :params => {:requests_per => 86399}).should have_selector('div#error')
+      request("/home/rps_calc", :method => "POST", :params => {:requests_per => 86399}).should have_selector('div#error')
     end
   end
 
   describe "converting into RPD" do
     it "should return 8.64 million reqs/day for a input of 100 reqs/sec" do
-      request("/home/rps_calc", :params => {:requests_per => 100, :convert_to => 'rpd'}).should contain('100 reqs/sec is 8640000 reqs/day')
+      request("/home/rps_calc", :method => "POST", :params => {:requests_per => 100, :convert_to => 'rpd'}).should contain('100 reqs/sec is 8640000 reqs/day')
     end
   end
 
